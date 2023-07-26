f597cd02b35cd559f3c1f3fe3eee668fb21ba046
caniko <git@rotas.mozmail.com>
2023-07-26 12:45:23 +0200

    Fix: try...catch, issue with 1.23 & 1.24 version of numpy

--- pydantic_numpy/util.py
+++ pydantic_numpy/util.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy.typing as npt
 from numpy.core._exceptions import UFuncTypeError
-from numpy.exceptions import DTypePromotionError
+from semver import Version
 
 
 def np_general_all_close(arr_a: npt.NDArray, arr_b: npt.NDArray, rtol: float = 1e-05, atol: float = 1e-08) -> bool:
@@ -21,9 +21,26 @@ def np_general_all_close(arr_a: npt.NDArray, arr_b: npt.NDArray, rtol: float = 1
     -------
     Bool
     """
-    try:
-        return np.allclose(arr_a, arr_b, rtol=rtol, atol=atol, equal_nan=True)
-    except UFuncTypeError:
-        return np.allclose(arr_a.astype(np.float64), arr_b.astype(np.float64), rtol=rtol, atol=atol, equal_nan=True)
-    except DTypePromotionError:
-        return bool(np.all(arr_a == arr_b))
+    return _np_general_all_close(arr_a, arr_b, rtol, atol)
+
+
+if Version.parse(np.version.version) < Version.parse("1.25.0"):
+
+    def _np_general_all_close(arr_a: npt.NDArray, arr_b: npt.NDArray, rtol: float = 1e-05, atol: float = 1e-08) -> bool:
+        try:
+            return np.allclose(arr_a, arr_b, rtol=rtol, atol=atol, equal_nan=True)
+        except UFuncTypeError:
+            return np.allclose(arr_a.astype(np.float64), arr_b.astype(np.float64), rtol=rtol, atol=atol, equal_nan=True)
+        except TypeError:
+            return bool(np.all(arr_a == arr_b))
+
+else:
+    from numpy.exceptions import DTypePromotionError
+
+    def _np_general_all_close(arr_a: npt.NDArray, arr_b: npt.NDArray, rtol: float = 1e-05, atol: float = 1e-08) -> bool:
+        try:
+            return np.allclose(arr_a, arr_b, rtol=rtol, atol=atol, equal_nan=True)
+        except UFuncTypeError:
+            return np.allclose(arr_a.astype(np.float64), arr_b.astype(np.float64), rtol=rtol, atol=atol, equal_nan=True)
+        except DTypePromotionError:
+            return bool(np.all(arr_a == arr_b))
