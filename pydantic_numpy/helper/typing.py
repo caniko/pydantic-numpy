d5f9e02a80230825b317973f90cd4d38a5a0d852
caniko <git@rotas.mozmail.com>
2023-12-22 09:21:01 +0100

    fix(serialization): JSON string serialization of models error fix test: Updated tests accordingly test(ci): Automatic upload to PyPi upon merge chore(version): Bump PATCH

---
+++ pydantic_numpy/helper/typing.py
@@ -0,0 +1,6 @@
+from typing_extensions import TypedDict
+
+
+class NumpyDataDict(TypedDict):
+    data_type: str
+    data: list
--- tests/test_pydantic.py
+++ tests/test_pydantic.py
@@ -1,5 +1,16 @@
-from tests.model import NpNDArrayModel
+import unittest
+
+import numpy as np
+
+from tests.model import NpNDArrayModel, NpNDArrayModelWithNonArray
+
+test_model_instance = NpNDArrayModelWithNonArray(array=np.zeros(10), non_array=2)
 
 
-def test_model_json_schema():
-    assert NpNDArrayModel.model_json_schema()
+class TestModelValidation(unittest.TestCase):
+    def test_model_json_schema(self):
+        self.assertTrue(NpNDArrayModel.model_json_schema())
+
+    def test_validate_json(self):
+        json_str = test_model_instance.model_dump_json()
+        self.assertTrue(test_model_instance.model_validate_json(json_str))
