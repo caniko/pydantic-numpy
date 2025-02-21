1db581105c95b815514b861c200348d4c48956dd
Sergei Nikiforov <sergei@nekohealth.com>
2025-02-21 13:52:32 +0100

    fix(typing): fix to numpy 2.2 typing notation: use tuple[str, ..] dimension everywhere chore(version): bump revision

--- pydantic_numpy/typing/type_safe/n_dimensional.py
+++ pydantic_numpy/typing/type_safe/n_dimensional.py
@@ -5,97 +5,97 @@ import numpy as np
 from pydantic_numpy.helper.annotation import NpArrayPydanticAnnotation
 
 NpNDArray: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[Any]],
+    np.ndarray[tuple[int, ...], np.dtype[Any]],
     NpArrayPydanticAnnotation.factory(data_type=None, dimensions=None, strict_data_typing=False),
 ]
 
 NpNDArrayInt64: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.int64]],
+    np.ndarray[tuple[int, ...], np.dtype[np.int64]],
     NpArrayPydanticAnnotation.factory(data_type=np.int64, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayInt32: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.int32]],
+    np.ndarray[tuple[int, ...], np.dtype[np.int32]],
     NpArrayPydanticAnnotation.factory(data_type=np.int32, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayInt16: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.int16]],
+    np.ndarray[tuple[int, ...], np.dtype[np.int16]],
     NpArrayPydanticAnnotation.factory(data_type=np.int16, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayInt8: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.int8]],
+    np.ndarray[tuple[int, ...], np.dtype[np.int8]],
     NpArrayPydanticAnnotation.factory(data_type=np.int8, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayUint64: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.uint64]],
+    np.ndarray[tuple[int, ...], np.dtype[np.uint64]],
     NpArrayPydanticAnnotation.factory(data_type=np.uint64, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayUint32: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.uint32]],
+    np.ndarray[tuple[int, ...], np.dtype[np.uint32]],
     NpArrayPydanticAnnotation.factory(data_type=np.uint32, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayUint16: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.uint16]],
+    np.ndarray[tuple[int, ...], np.dtype[np.uint16]],
     NpArrayPydanticAnnotation.factory(data_type=np.uint16, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayUint8: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.uint8]],
+    np.ndarray[tuple[int, ...], np.dtype[np.uint8]],
     NpArrayPydanticAnnotation.factory(data_type=np.uint8, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayFpLongDouble: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.longdouble]],
+    np.ndarray[tuple[int, ...], np.dtype[np.longdouble]],
     NpArrayPydanticAnnotation.factory(data_type=np.longdouble, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayFp64: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.float64]],
+    np.ndarray[tuple[int, ...], np.dtype[np.float64]],
     NpArrayPydanticAnnotation.factory(data_type=np.float64, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayFp32: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.float32]],
+    np.ndarray[tuple[int, ...], np.dtype[np.float32]],
     NpArrayPydanticAnnotation.factory(data_type=np.float32, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayFp16: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.float16]],
+    np.ndarray[tuple[int, ...], np.dtype[np.float16]],
     NpArrayPydanticAnnotation.factory(data_type=np.float16, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayComplexLongDouble: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.clongdouble]],
+    np.ndarray[tuple[int, ...], np.dtype[np.clongdouble]],
     NpArrayPydanticAnnotation.factory(data_type=np.clongdouble, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayComplex128: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.complex128]],
+    np.ndarray[tuple[int, ...], np.dtype[np.complex128]],
     NpArrayPydanticAnnotation.factory(data_type=np.complex128, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayComplex64: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.complex64]],
+    np.ndarray[tuple[int, ...], np.dtype[np.complex64]],
     NpArrayPydanticAnnotation.factory(data_type=np.complex64, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayBool: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.bool_]],
+    np.ndarray[tuple[int, ...], np.dtype[np.bool_]],
     NpArrayPydanticAnnotation.factory(data_type=np.bool_, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayDatetime64: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.datetime64]],
+    np.ndarray[tuple[int, ...], np.dtype[np.datetime64]],
     NpArrayPydanticAnnotation.factory(data_type=np.datetime64, dimensions=None, strict_data_typing=True),
 ]
 
 NpNDArrayTimedelta64: TypeAlias = Annotated[
-    np.ndarray[Any, np.dtype[np.timedelta64]],
+    np.ndarray[tuple[int, ...], np.dtype[np.timedelta64]],
     NpArrayPydanticAnnotation.factory(data_type=np.timedelta64, dimensions=None, strict_data_typing=True),
 ]
 
--- pydantic_numpy/typing/with_loader/n_dimensional.py
+++ pydantic_numpy/typing/with_loader/n_dimensional.py
@@ -7,97 +7,97 @@ from pydantic_numpy.helper.annotation import NpArrayPydanticAnnotation
 from pydantic_numpy.model import MultiArrayNumpyFile
 
 NpLoadingNDArray: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[Any]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[Any]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=None, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayInt64: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.int64]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.int64]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.int64, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayInt32: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.int32]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.int32]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.int32, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayInt16: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.int16]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.int16]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.int16, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayInt8: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.int8]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.int8]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.int8, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayUint64: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.uint64]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.uint64]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.uint64, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayUint32: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.uint32]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.uint32]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.uint32, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayUint16: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.uint16]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.uint16]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.uint16, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayUint8: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.uint8]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.uint8]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.uint8, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayFpLongDouble: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.longdouble]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.longdouble]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.longdouble, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayFp64: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.float64]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.float64]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.float64, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayFp32: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.float32]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.float32]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.float32, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayFp16: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.float16]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.float16]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.float16, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayComplexLongDouble: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.clongdouble]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.clongdouble]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.clongdouble, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayComplex128: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.complex128]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.complex128]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.complex128, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayComplex64: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.complex64]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.complex64]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.complex64, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayBool: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.bool_]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.bool_]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.bool_, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayDatetime64: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.datetime64]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.datetime64]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.datetime64, dimensions=None, strict_data_typing=False),
 ]
 
 NpLoadingNDArrayTimedelta64: TypeAlias = Annotated[
-    Union[np.ndarray[Any, np.dtype[np.timedelta64]], FilePath, MultiArrayNumpyFile],
+    Union[np.ndarray[tuple[int, ...], np.dtype[np.timedelta64]], FilePath, MultiArrayNumpyFile],
     NpArrayPydanticAnnotation.factory(data_type=np.timedelta64, dimensions=None, strict_data_typing=False),
 ]
 
