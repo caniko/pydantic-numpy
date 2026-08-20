458b7438fd384bd51e3905fff39c6992aac769ee
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-20 13:32:01 +0200

    chore: bump harbor pins to new rev

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     py-harbor = {
-      url = "git+https://codeberg.org/caniko/py-harbor.git?ref=trunk&rev=c4bcdbc27f88cc8eb4056b06d5f69a5dec63ff28";
+      url = "git+https://codeberg.org/caniko/py-harbor.git?ref=trunk&rev=9a6231b80acdaa29e2d2bd7145db08c71ddf1fd1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
