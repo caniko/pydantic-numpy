127efb02e8d6bd0f144f188c95b0f093ee8dac1d
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-25 02:20:29 +0200

    build: use canonical GitHub inputs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     py-harbor = {
-      url = "git+https://github.com/caniko/harbor-py.git?ref=trunk&rev=c4bcdbc27f88cc8eb4056b06d5f69a5dec63ff28";
+      url = "git+https://github.com/caniko/harbor-py.git?ref=trunk&rev=a9396b7334869576ab46191ca7b34a9c63878be5";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
